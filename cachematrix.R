39d75e57a398acf8e30a330d1c1823c26fd746eb
airgradient <western_zhou@pku.edu.cn>
2017-11-26 16:18:01 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        InvMat <- NULL
+        set <- function(y){
+                x <<- y
+                InvMat <<- NULL
+        }
+        get <- function() x
+        setInvMat <- function(pivy) InvMat <<- pivy
+        getInvMat <- function() InvMat
+        list(set = set, get = get. setmean = setmean, getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        InvMat <- x$getInvMat()
+        if(!is.null(InvMat)){
+                message("getting saved inverse matrix")
+                return(InvMat)
+        }
+        oriMat <- x$get()
+        InvMat <- solve(oriMat)
+        x$setInvMat(InvMat)
+        InvMat
         ## Return a matrix that is the inverse of 'x'
 }
