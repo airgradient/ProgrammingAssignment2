cf8c95968d18801ea9da9937a117451b2c25b0d1
airgradient <western_zhou@pku.edu.cn>
2017-11-26 16:37:21 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,29 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function would retrieve the value of a matrix. If there is an inverse matrix, they can just store it inside the makeCacheMatrix   
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  InvMat <- NULL
+  set <- function(y){
+    x <<- y
+    InvMat <<- NULL
+  }
+  get <- function() x
+  setInvMat <- function(pivy) InvMat <<- pivy
+  getInvMat <- function() InvMat
+  list(set = set, get = get, setInvMat = setInvMat, getInvMat = getInvMat)
 }
 
-
-## Write a short comment describing this function
-
+## This function could calculate the inverse matrix of a non-stored one, and return the value of a stored one.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  InvMat <- getInvMat(x)
+  if(!is.null(InvMat)){
+    message("getting saved inverse matrix")
+    return(InvMat)
+  }
+  oriMat <- get(x)
+  InvMat <- solve(oriMat)
+  x$setInvMat(InvMat)
+  InvMat
 }
